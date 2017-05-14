0ef39937a3cdf3d9c8df5ff2b34c5fe317a512d7
TeddyGolosov <glabgtodor@mail.ru>
2017-05-14 23:59:10 +0300

    Create my_app.py

---
+++ my_app.py
@@ -0,0 +1,37 @@
+import flask
+import telebot
+import conf
+
+WEBHOOK_URL_BASE = "https://{}:{}".format(conf.WEBHOOK_HOST, conf.WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(conf.TOKEN)
+
+bot = telebot.TeleBot(conf.TOKEN, threaded=False)  # бесплатный аккаунт pythonanywhere запрещает работу с несколькими тредами
+
+# удаляем предыдущие вебхуки, если они были
+bot.remove_webhook()
+
+# ставим новый вебхук = Слышь, если кто мне напишет, стукни сюда — url
+bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH)
+
+app = flask.Flask(__name__)
+def send_welcome(message):
+	bot.send_message(message.chat.id, "Здравствуйте! Это бот, который считает, сколько у Вас слов в сообщении.")
+@bot.message_handler(func=lambda m: True)  # этот обработчик реагирует на любое сообщение
+def send_len(message):
+	bot.send_message(message.chat.id, 'В вашем сообщении {} слов.'.format(len(message.text.split(' '))))	
+
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return 'ok'
+
+
+# обрабатываем вызовы вебхука = функция, которая запускается, когда к нам постучался телеграм 
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
